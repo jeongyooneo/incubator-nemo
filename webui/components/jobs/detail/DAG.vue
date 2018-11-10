d574951f04e142f1773ed3d374812d320eb1e01f
Won Wook SONG <wsong0512@gmail.com>
2018-11-10 15:23:06 +0900

    add labels, debug why it doesnt show vertex details

--- webui/components/jobs/detail/DAG.vue
+++ webui/components/jobs/detail/DAG.vue
@@ -30,15 +30,19 @@ import { STATE } from '../../../assets/constants';
 
 const DEBOUNCE_INTERVAL = 200;
 
-const VERTEX_WIDTH = 50;
-const VERTEX_HEIGHT = 50;
-const VERTEX_RADIUS = 4;
-const PAN_MARGIN = 20;
+const STAGE_VERTEX_WIDTH = 50;
+const STAGE_VERTEX_HEIGHT = 50;
 const RECT_ROUND_RADIUS = 4;
 const RECT_STROKE_WIDTH = 2;
-const INTER_STAGE_EDGE_STROKE_WIDTH = 2;
+
+const VERTEX_SQUARE_SIDE = 20;
+const VERTEX_DOT_RADIUS = 7;
+const PAN_MARGIN = 20;
+const INTER_STAGE_EDGE_STROKE_WIDTH = 3;
+const INTRA_STAGE_EDGE_STROKE_WIDTH = 3;
 const ARROW_SIDE = 3;
-const FONT_SIZE = 15;
+const VERTEX_FONT_SIZE = 15;
+const STAGE_FONT_SIZE = 20;
 
 const GRAPH_MARGIN = 15;
 
@@ -99,6 +103,8 @@ export default {
       stageInnerObjects: {},
       // array of stage label text object
       stageTextObjects: [],
+      // array of vertex label text object
+      vertexTextObjects: [],
     };
   },
 
@@ -478,7 +484,7 @@ export default {
      */
     rearrangeFontSize(ratio) {
       this.stageTextObjects.forEach(text => {
-        text.set('fontSize', FONT_SIZE * ratio);
+        text.set('fontSize', STAGE_FONT_SIZE * ratio);
       });
     },
 
@@ -539,10 +545,12 @@ export default {
         g.setDefaultEdgeLabel(function () { return {}; });
 
         innerVertices.forEach(vertex => {
+          let label = vertex.properties.class === "OperatorVertex" ?
+            vertex.properties.transform.split("Transform:")[0] : vertex.properties.class;
           g.setNode(vertex.id, {
-            label: vertex.id,
-            width: VERTEX_WIDTH,
-            height: VERTEX_HEIGHT,
+            label: label,
+            width: STAGE_VERTEX_WIDTH,
+            height: STAGE_VERTEX_HEIGHT,
           });
         });
 
@@ -562,7 +570,7 @@ export default {
         g.nodes().map(node => g.node(node)).forEach(vertex => {
           let vertexCircle = new fabric.Circle({
             metricId: vertex.label,
-            radius: VERTEX_RADIUS,
+            radius: VERTEX_DOT_RADIUS,
             left: vertex.x,
             top: vertex.y,
             originX: 'center',
@@ -574,9 +582,23 @@ export default {
             lockMovementY: true,
           });
 
+          let top = vertex.label.length > 10 ?
+            vertex.y + (vertex.height * 5 / 12) : vertex.y + (vertex.height * 7 / 24);
+          let vertexLabelObj = new fabric.Text(vertex.label, {
+            left: vertex.x,
+            top: top,
+            fontSize: VERTEX_FONT_SIZE,
+            originX: 'center',
+            originY: 'center',
+            selectable: false,
+          });
+          this.vertexTextObjects.push(vertexLabelObj);
+
           this.vertexObjects[vertex.label] = vertexCircle;
           this.stageInnerObjects[stageId].push(vertexCircle);
+          this.stageInnerObjects[stageId].push(vertexLabelObj);
           objectArray.push(vertexCircle);
+          objectArray.push(vertexLabelObj);
         });
 
         // create internal edges
@@ -588,7 +610,7 @@ export default {
             metricId: edge.label,
             fill: 'transparent',
             stroke: 'black',
-            strokeWidth: 2,
+            strokeWidth: INTRA_STAGE_EDGE_STROKE_WIDTH,
             perPixelTargetFind: true,
             hasControls: false,
             hasRotatingPoint: false,
@@ -651,8 +673,8 @@ export default {
 
         let stageLabelObj = new fabric.Text(stage.label, {
           left: stage.x,
-          top: stage.y - stage.height / 3,
-          fontSize: FONT_SIZE,
+          top: stage.y - (stage.height / 2) + (STAGE_FONT_SIZE / 3),
+          fontSize: STAGE_FONT_SIZE,
           originX: 'center',
           originY: 'center',
           selectable: false,
